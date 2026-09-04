c11bfeb3c9b58471392be5111ef0ee28ea29e3b7
Matthew Galvin <videohead@hotmail.com>
2026-09-04 12:57:02 -0700

    Permissions and config files updated

--- headless/next.config.ts
+++ headless/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const wordpressHostname = process.env.WORDPRESS_HOSTNAME || "localhost";
 
 const nextConfig: NextConfig = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || undefined,
   images: {
     remotePatterns: [
       {
